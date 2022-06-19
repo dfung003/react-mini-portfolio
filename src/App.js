e53c462a6dd91b4632eac04289f6c1e198cd1666
dfung003 <dfung003@ucr.edu>
2022-06-18 18:29:47 -0700

    navbar linking to proper pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import { useState } from "react";
 
 function App() {
+  const [info, setInfo] = useState(null)
   return (
     <div className="App">
       <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
+import { Link } from 'react-router-dom';
+
+
 export default function Header() {
     return (
-        <div className="nav">
-            <h1>This is Header</h1>
-        </div>
+        <nav className="nav">
+            <Link className="nav-link" to="/">
+                <h1>Damon Fung</h1>
+            </Link>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/dfung003">
+                <div>GitHub</div>
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/damonfung27/">
+                <div>LinkedIn</div>
+            </a>
+        </nav>
     )
 
 }

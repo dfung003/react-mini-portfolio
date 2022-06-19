4a3452e3609c9ac7dafb8c3a588e61edfadcaf6a
dfung003 <dfung003@ucr.edu>
2022-06-18 18:05:31 -0700

    components rendering

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="nav">
+            <h1>This is Header</h1>
+        </div>
+    )
+
+}
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import Projects from "./Projects"
+
+export default function Main() {
+    return (
+        <div className="main">
+            <h1>This is Main</h1>
+            <Projects />
+        </div>
+    )
+}
